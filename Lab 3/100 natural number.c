d9c2702e9fbec433d8d3b95565489b320caeae44
Shiva Raj Paudel <official.beexoul@gmail.com>
2024-12-23 22:19:12 +0545

    new uploads

--- Lab 3/100 natural number.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int i;
-    for(i=0;i<=100;i++)  //Loop that runs for 100 time 
-    {
-    printf("%d \t",i);   //Print the output of loop 
-    }
-    return 0;
-}
--- Lab 3/sum.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-int a=10,b=15,sum;
-sum=a+b;
-printf("%d\n",sum);
-    return 0;
-}
